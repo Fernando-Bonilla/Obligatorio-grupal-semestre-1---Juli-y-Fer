18eb1ed12a33d993d7d0ea3f92ef394b00caacd4
Fernando <fernandobonilla832@gmail.com>
2024-06-10 17:44:34 -0300

    Added class with the big array with all games

--- trabajo-grupal/js/admin.js
+++ trabajo-grupal/js/admin.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {      
-
+document.addEventListener('DOMContentLoaded', () => {  
+    
     let titleCrearJuego = document.getElementById('title-crear-juego');
     let altaFormContainer = document.getElementById('alta-form-container');
 
@@ -9,9 +9,11 @@ document.addEventListener('DOMContentLoaded', () => {
     let addGameButton = document.getElementById('crear-juego');
     addGameButton.addEventListener('click', createGame);
 
-    function createGame() {       
-
-        //let id = arrayOfGames.length + 1;
+    function createGame() {     
+        let arrayOfGames = JSON.parse(localStorage.getItem('JUEGOS')); 
+        console.log(arrayOfGames)
+                 
+        let id = arrayOfGames.length + 1;
         let name = document.getElementById('formGroupExampleInput').value;
         let price = document.getElementById('formGroupExampleInput2').value;
         let description = document.getElementById('formGroupExampleInput3').value;
@@ -19,14 +21,14 @@ document.addEventListener('DOMContentLoaded', () => {
         let clasificacion = document.getElementById('formGroupExampleInput5').value;
         let imgSrc = document.getElementById('formFileCreateGame').value;
 
-        let game = {id: 500, name: name, price: price, description: description, imgSrc: imgSrc, category: category, clasificacion: clasificacion};
+        let game = {id: id, name: name, price: price, description: description, imgSrc: imgSrc, category: category, clasificacion: clasificacion};
         //let game = new Game(name, price, description, imgSrc, category, clasificacion);
         
-        let arrayOfGames = JSON.parse(localStorage.getItem('JUEGOS'));
+        
+        console.log(arrayOfGames);
         arrayOfGames.push(game);           
         localStorage.setItem('JUEGOS', JSON.stringify(arrayOfGames));
-        alert('Juego creado');
-        console.log(arrayOfGames);        
+        alert('Juego creado');         
         
     }       
     
--- trabajo-grupal/js/script.js
+++ trabajo-grupal/js/script.js
@@ -1,29 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {    
+document.addEventListener('DOMContentLoaded', () => {          
+    
     let cart = JSON.parse(localStorage.getItem('cart')) || [];
-    let GAMES = JSON.parse(localStorage.getItem('JUEGOS')); // usamos el nombre GAMES porque movimos esta variable a un archivo separado, y en todas las funciones estabamos usando este nombre de variable   
+
+    let GAMES = JSON.parse(localStorage.getItem('JUEGOS')) || []; // usamos el nombre GAMES porque movimos esta variable a un archivo separado, y en todas las funciones estabamos usando este nombre de variable   
     console.log(GAMES)
-    /*const GAMES = [
-        { id: 1, name: 'Far Cry 6', price: 10, description: "Un juego emocionante lleno de aventuras.", imgSrc: "img/far_cry_6.jpg", category: "Accion", clasificacion: "E"},
-        { id: 2, name: 'Far Cry 5', price: 15, description: "Mundo abierto lleno de acción.", imgSrc: "img/far_cry_5.jpg", category: "Accion", clasificacion: "E 10+"},
-        { id: 3, name: 'Watch Dogs', price: 20, description: "Mundo genuinamente lleno de codigo.", imgSrc: "img/watch.jpg", category: "Accion", clasificacion: "T"},
-        { id: 4, name: 'Star Wars', price: 12, description: "Aventura épica en mundos místico.", imgSrc: "img/star-wars.jpg", category: "Estrategia", clasificacion: "M" },
-        { id: 5, name: 'CSGO 2', price: 18, description: "Mejor juego de disparos del mundo.", imgSrc: "img/counterstrike_2.jpg", category: "Shooter", clasificacion: "A" },
-        { id: 6, name: 'CS 1.6', price: 22, description: "Disparos en primera persona multijugador.", imgSrc: "img/cs1.6.jpg", category: "Shooter", clasificacion: "A" },
-        { id: 7, name: 'DOOM', price: 25, description: "Acción trepidante y combates espectaculares.", imgSrc: "img/doom.jpg", category: "Estrategia", clasificacion: "" },
-        { id: 8, name: 'Sims 4', price: 16, description: "Juego de simulación muy realista en personas.", imgSrc: "img/sims4.jpg", category: "Estrategia", clasificacion: "" },
-        { id: 9, name: 'ARK', price: 19, description: "Mundo lleno de misterios y aventuras inolvidables.", imgSrc: "img/ark.jpg", category: "Estrategia", clasificacion: "" },
-        { id: 10, name: 'Assetto Corsa', price: 14, description: "Juego de carreras con vehículos personalizables.", imgSrc: "img/asseto.jpg", category: "Estrategia", clasificacion: "" },
-        { id: 11, name: 'ZUMA', price: 8, description: "Puzzle adictivo con desafíos diarios.", imgSrc: "img/zuma.jpg", category: "Estrategia", clasificacion: "" },
-        { id: 12, name: 'FIFA 24', price: 24, description: "Simulación deportiva con los mejores equipos del mundo.", imgSrc: "img/fifa24.jpg", category: "Estrategia", clasificacion: "" },
-        { id: 13, name: 'Phasmophobia', price: 20, description: "Juego de terror que te mantendrá en suspenso.", imgSrc: "img/phasmophobia.jpg", category: "Estrategia", clasificacion: "" },
-        { id: 14, name: 'Cities Skylines', price: 17, description: "Construcción de ciudades con infinitas posibilidades.", imgSrc: "img/cities-skylines.jpg", category: "Estrategia", clasificacion: "" },
-        { id: 15, name: 'Undawn', price: 23, description: "Supervivencia en un mundo postapocalíptico.", imgSrc: "img/undawn.jpg", category: "Estrategia", clasificacion: ""},
-        { id: 16, name: 'WOW', price: 26, description: "Juego de rol multijugador en línea.", imgSrc: "img/wow.jpg", category: "Estrategia", clasificacion: "M"},
-        { id: 17, name: 'GTA 5', price: 13, description: "Mundo abierto con infinitas cosas para hacer.", imgSrc: "img/gta5.jpg", category: "Estrategia", clasificacion: "" },
-        { id: 18, name: 'Guitar Hero 3', price: 11, description: "Juego de música con desafíos emocionantes.", imgSrc: "img/guitarhero.jpg", category: "Musica", clasificacion: "" },
-        { id: 19, name: 'RUST', price: 9, description: "Supervivencia realista lleno de peligros.", imgSrc: "img/rust.jpg", category: "Estrategia", clasificacion: "" },
-        { id: 20, name: 'CALL OF DUTY', price: 18, description: "Disparos en primera persona.", imgSrc: "img/Black_Ops_2.png", category: "Estrategia", clasificacion: "" },
-    ];*/
+    if(GAMES.length == 0) {
+        GAMES = Preload.GAMES 
+        localStorage.setItem('JUEGOS', JSON.stringify(GAMES))
+    }    
 
     function showGamesList(gamesData) {  
 
